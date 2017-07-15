42ebc142011a4af9129a0d8500b7d955c83fad17
CroyDaniel <dsc986@tamu.edu>
2017-07-15 13:28:12 -0500

    Added prompt for incorrect command-line arguments

--- word_frequency_hash.rb
+++ word_frequency_hash.rb
@@ -2,6 +2,12 @@
 # Date : 07/15/17
 # Word Frequency 
 
+if ARGV.length != 1
+	puts "Incorrect number of arguments."
+	puts "Usage: ruby wordFrequencyList.rb InputFile.txt\n"
+	exit
+end
+
 fileName = ARGV[0]
 file = File.open(fileName, "r")
 counter = {}
