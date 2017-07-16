4ec1df8d99cfdc56694327bf2202ed5317638d47
CroyDaniel <dsc986@tamu.edu>
2017-07-16 14:32:17 -0500

    Added error handling for when file does not exist; added comments for code whose purpose may be unclear.

--- word_frequency_hash.rb
+++ word_frequency_hash.rb
@@ -8,8 +8,16 @@ if ARGV.length != 1
 	exit
 end
 
-fileName = ARGV[0]
-file = File.open(fileName, "r")
+file_name = ARGV[0]
+
+begin
+	file = File.open(file_name, "r")
+rescue Errno::ENOENT # ENOENT is the error number thrown when the specified file does not exist
+	puts "File: \"#{file_name}\" does not exist."
+	puts "Please make sure you are opening the correct file.\n"
+	exit
+end
+	
 counter = {}
 while !file.eof?
 	line = file.readline
@@ -22,8 +30,13 @@ while !file.eof?
 		end
 	end
 end
-longest_word = counter.keys.max_by {|word| word.length}
+
+file.close()
+
+longest_word = counter.keys.max_by {|word| word.length} # this value is used to left-justify each word according to the length of the longest word
 counter.each do |word, count|
 	puts "#{word}".ljust(longest_word.length) + " : #{count}"
 end
+
+
 
